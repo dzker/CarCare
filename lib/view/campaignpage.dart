6435f2d4db4c3a395d2ec53fcd41464ded00d99d
HARITH DZIKRI BIN HASHIM <harithdzikri12@gmail.com>
2024-04-09 17:30:29 +0800

    update campaingpage
    added tab bar in the page

--- lib/view/campaignpage.dart
+++ lib/view/campaignpage.dart
@@ -7,230 +7,79 @@ class CampaignPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-                gradient: LinearGradient(begin: Alignment.topCenter, colors: [
-              Colors.green,
-              Colors.green,
-              Colors.green,
-            ])),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                const SizedBox(
-                  height: 40,
+      appBar: AppBar(
+        title: FadeInUp(
+          child: const Text(
+            'What\'s New Today? ',
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.w700,
+            ), //change variable for name here
+          ),
+        ),
+        backgroundColor: Colors.green,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              colors: [
+                Colors.green,
+                Colors.green,
+                Colors.green,
+              ],
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: TextField(
+                  style: const TextStyle(color: Colors.black, fontSize: 15),
+                  decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: BorderSide.none),
+                      hintText: 'e.g: Hari Raya Promo',
+                      suffixIcon: const Icon(Icons.search)),
                 ),
-                Center(
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        FadeInUp(
-                          child: const Text(
-                            "Campaign Page",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 30,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 40),
-                Container(
+              ),
+              const SizedBox(height: 10),
+              DefaultTabController(
+                length: 3,
+                child: Container(
                   width: double.maxFinite,
                   decoration: const BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(60),
-                          topRight: Radius.circular(60))),
-                  child: const Padding(
-                    padding: EdgeInsets.symmetric(
-                        vertical: 85), // Padding for Height of name
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text("Samad Said",
-                              style: TextStyle(
-                                  fontSize: 30, fontWeight: FontWeight.bold)),
-                          Text(
-                            "10 years experience in MARDI\n        Aim to be different",
-                            style: TextStyle(fontSize: 20),
-                          )
-                        ]),
+                          topLeft: Radius.circular(25),
+                          topRight: Radius.circular(25))),
+                  child: const TabBar(
+                    tabs: <Widget>[
+                      Tab(
+                        text: 'Featured',
+                      ),
+                      Tab(
+                        text: 'Promotion',
+                      ),
+                      Tab(
+                        text: 'Event',
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
-          ),
-          Positioned.fill(
-            //Position efficienly the card
-            top: 350,
-            child: Container(
-              color: Colors.white,
-              child: ListView(
-                children: [
-                  GestureDetector(
-                    // onTap: () {
-                    //   Navigator.push(
-                    //       context,
-                    //       MaterialPageRoute(
-                    //           builder: (context) => EditProfilePage()));
-                    // },
-                    child: Card(
-                      color: Colors.grey,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30)),
-                      child: const ListTile(
-                        leading: Icon(
-                          Icons.person_2_outlined,
-                          color: Colors.black,
-                        ),
-                        title: Text(
-                          'Edit Profile',
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-                        trailing: Icon(
-                          Icons.arrow_forward_ios_outlined,
-                          color: Colors.black54,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  GestureDetector(
-                    // onTap: () {
-                    //   _launchFAQURL();
-                    // },
-                    child: Card(
-                      color: Colors.grey,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30)),
-                      child: const ListTile(
-                        leading: Icon(
-                          Icons.question_mark_rounded,
-                          color: Colors.black,
-                        ),
-                        title: Text(
-                          'FAQ',
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-                        trailing: Icon(
-                          Icons.arrow_forward_ios_outlined,
-                          color: Colors.black54,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  GestureDetector(
-                    // onTap: () {
-                    //   Navigator.push(
-                    //       context,
-                    //       MaterialPageRoute(
-                    //           builder: (context) => const HistoryPage()));
-                    // },
-                    child: Card(
-                      color: Colors.grey,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30)),
-                      child: const ListTile(
-                        leading: Icon(
-                          Icons.history_rounded,
-                          color: Colors.black,
-                        ),
-                        title: Text(
-                          'History',
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-                        trailing: Icon(
-                          Icons.arrow_forward_ios_outlined,
-                          color: Colors.black54,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  GestureDetector(
-                    // onTap: () {
-                    //   Navigator.push(
-                    //       context,
-                    //       MaterialPageRoute(
-                    //           builder: (context) => const LoginScreen()));
-                    // },
-                    child: Card(
-                      color: Colors.grey,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30)),
-                      child: const ListTile(
-                        leading: Icon(
-                          Icons.logout,
-                          color: Colors.black,
-                        ),
-                        title: Text(
-                          'Logout',
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-                        trailing: Icon(
-                          Icons.arrow_forward_ios_outlined,
-                          color: Colors.black54,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
               ),
-            ),
+            ],
           ),
-          // Column(
-          //   //column for Circle Avatar
-          //   children: [
-          //     const SizedBox(
-          //       height: 90,
-          //     ),
-          //     SizedBox(
-          //       height: 130,
-          //       child: Row(
-          //         mainAxisAlignment: MainAxisAlignment.center,
-          //         children: [
-          //           Container(
-          //             padding: const EdgeInsets.all(1),
-          //             decoration: BoxDecoration(
-          //                 shape: BoxShape.circle,
-          //                 border: Border.all(
-          //                     color: Colors.yellow.shade600, width: 3)),
-          //             child: const CircleAvatar(
-          //               maxRadius: 65,
-          //               backgroundImage: AssetImage("assets/images/samad.jpg"),
-          //               backgroundColor: Colors.black,
-          //             ),
-          //           )
-          //         ],
-          //       ),
-          //     ),
-          //   ],
-          // ),
-        ],
+        ),
       ),
     );
   }
