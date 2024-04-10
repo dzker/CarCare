81acce351af60d6ed9e58e38186f9cab523ba5d4
HARITH DZIKRI BIN HASHIM <harithdzikri12@gmail.com>
2024-04-11 02:10:27 +0800

    update campaing.page dart

--- lib/view/campaignpage.dart
+++ lib/view/campaignpage.dart
@@ -3,18 +3,40 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class CampaignPage extends StatelessWidget {
+class CampaignPage extends StatefulWidget {
+  const CampaignPage({super.key});
+
+  @override
+  State<CampaignPage> createState() => _CampaignPageState();
+}
+
+class _CampaignPageState extends State<CampaignPage>
+    with TickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: FadeInUp(
           child: const Text(
-            'What\'s New Today? ',
+            "What's New Today?",
             style: TextStyle(
               fontSize: 25,
               fontWeight: FontWeight.w700,
-            ), //change variable for name here
+            ),
           ),
         ),
         backgroundColor: Colors.green,
@@ -35,46 +57,61 @@ class CampaignPage extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              const SizedBox(
-                height: 10,
-              ),
+              const SizedBox(height: 10),
               Padding(
                 padding: const EdgeInsets.all(15.0),
                 child: TextField(
                   style: const TextStyle(color: Colors.black, fontSize: 15),
                   decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide: BorderSide.none),
-                      hintText: 'e.g: Hari Raya Promo',
-                      suffixIcon: const Icon(Icons.search)),
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide.none,
+                    ),
+                    hintText: 'e.g: Hari Raya Promo',
+                    suffixIcon: const Icon(Icons.search),
+                  ),
                 ),
               ),
               const SizedBox(height: 10),
               DefaultTabController(
                 length: 3,
-                child: Container(
-                  width: double.maxFinite,
-                  decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
+                child: Column(
+                  children: [
+                    Container(
+                      width: double.maxFinite,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
                           topLeft: Radius.circular(25),
-                          topRight: Radius.circular(25))),
-                  child: const TabBar(
-                    tabs: <Widget>[
-                      Tab(
-                        text: 'Featured',
+                          topRight: Radius.circular(25),
+                        ),
                       ),
-                      Tab(
-                        text: 'Promotion',
+                      child: TabBar(
+                        controller: _tabController,
+                        tabs: const <Widget>[
+                          Tab(text: 'Event'),
+                          Tab(text: 'Promotion'),
+                          Tab(text: 'Vouchers'),
+                        ],
                       ),
-                      Tab(
-                        text: 'Event',
+                    ),
+                    Container(
+                      color: Colors.white,
+                      child: SizedBox(
+                        height: 500,
+                        child: TabBarView(
+                          controller: _tabController,
+                          children: const <Widget>[
+                            Center(child: Text('Hei')),
+                            Center(child: Text('Hzzz')),
+                            Center(child: Text('Huiiii')),
+                          ],
+                        ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ],
